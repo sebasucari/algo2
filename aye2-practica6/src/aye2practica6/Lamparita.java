017004bee879582e893eec7c01baca0a37a6f318
sebasucari <sebasucari@hotmail.com>
2018-05-21 10:06:08 -0300

    ej 1 practica 6

--- aye2-practica6/src/aye2practica6/Lamparita.java
+++ aye2-practica6/src/aye2practica6/Lamparita.java
@@ -1,6 +1,29 @@
 package aye2practica6;
-
+//1. Implemente la clase Lamparita, que sirva para representar el estado de encendido de
+//una lamparita (encendido o apagado). Defina, asimismo, dos métodos que permitan
+//encender y apagar la luz de la lamparita y otro que indique en qué estado se
+//encuentra. La lamparita inicialmente está apagada.
 public class Lamparita {
-
+	enum EstadoLampara {ENCENDIDA, APAGADA};
 	
+	private EstadoLampara estado;
+	
+	//privaet boolean estado;
+	
+	public Lamparita() {
+		estado = EstadoLampara.APAGADA;
+		
+	}
+	
+	public void encender() {
+		estado = EstadoLampara.ENCENDIDA;
+	}
+	
+	public void apagar() {
+		estado = EstadoLampara.APAGADA;
+	}
+	
+	public EstadoLampara getEstado() {
+		return estado;
+	}
 }
